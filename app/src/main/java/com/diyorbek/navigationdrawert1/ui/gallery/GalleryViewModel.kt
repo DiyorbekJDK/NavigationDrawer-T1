bd7fe7f8601fda943fe4a6a56113c49d1d1d5135
DiyorbekJDK <diyorbekdavronov07072007@gmail.com>
2023-01-10 14:06:15 +0500

    Banana

---
+++ app/src/main/java/com/diyorbek/navigationdrawert1/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.diyorbek.navigationdrawert1.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
